a2b3226fb82db60cd1ef275530838cde1ccd47bd
Avelino Bego <avelino.bego@gmail.com>
2025-11-25 19:00:24 -0300

    test: 🚦 alteração do XML

--- src/tests_dinamic.rs
+++ src/tests_dinamic.rs
@@ -5,6 +5,8 @@ fn test_dinamic() {
     let din: Node = r#"<indentificacao id="1" xmnls="xpto">
     <nome>Avelino</nome>
     <emprego/>
+    <salario ano="2025">10000.00</salario>
+    <demissao>2025-11-17</demissao>
     <identificadores id="identific">
     <cpf>16944301890</cpf>
     <cpf>16944301890</cpf>
