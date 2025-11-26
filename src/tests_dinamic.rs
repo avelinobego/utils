02097c4fe25f3bf503f2ca8a43c9bdc40f0306e4
Avelino Bego <avelino.bego@gmail.com>
2025-11-26 02:00:19 -0300

    test: 🚦 modificações no teste

--- src/tests_dinamic.rs
+++ src/tests_dinamic.rs
@@ -1,18 +1,26 @@
+
+
 #[test]
 fn test_dinamic() {
     use crate::dinamic::Node;
+    use std::fs;
 
-    let din: Node = r#"<indentificacao id="1" xmnls="xpto">
-    <nome>Avelino</nome>
-    <emprego/>
-    <salario ano="2025">10000.00</salario>
-    <demissao>2025-11-17</demissao>
-    <identificadores id="identific">
-    <cpf>16944301890</cpf>
-    <cpf>16944301890</cpf>
-    </identificadores>
-</indentificacao>"#
-        .into();
+    // let conteudo = fs::read_to_string("S1000.xml").expect("erro ao abrir arquivo");
+    // let din: Node = conteudo.into();
+
+        let din: Node = r#"<indentificacao id="1" xmnls="xpto">
+        <nome>Avelino</nome>
+        <emprego/>
+        <salario ano="2025">10000.00</salario>
+        <demissao>2025-11-17</demissao>
+        <identificadores id="identific">
+        <cpf>16944301890</cpf>
+        <cpf>16944301890</cpf>
+        </identificadores>
+    </indentificacao>"#
+            .into();
+
 
     println!("{:#?}", din);
+
 }
