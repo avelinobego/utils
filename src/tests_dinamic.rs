3ebcd4e750999f3c9602d5845aaf8c4de09312cf
Avelino Bego <avelino.bego@gmail.com>
2025-12-05 01:23:33 -0300

    test: 🚦 modificação testes

--- src/tests_dinamic.rs
+++ src/tests_dinamic.rs
@@ -1,95 +1,165 @@
+#[test]
+fn test_to_xml_float() {
+    use crate::dinamic::Node;
+
+    let node = Node {
+        name: "salario".into(),
+        value: Some(10000.1234567890.into()),
+        attributes: Vec::default(),
+        children: Vec::default(),
+    };
+
+    assert_eq!(node.to_xml(), "<salario>10000.123456789</salario>");
+}
+
+#[test]
+fn test_to_xml_year_month() {
+    use crate::dinamic::Node;
+
+    let node = Node {
+        name: "data".into(),
+        value: Some((2025, 1).into()),
+        attributes: Vec::default(),
+        children: Vec::default(),
+    };
+
+    assert_eq!(node.to_xml(), "<data>2025-01</data>");
+}
+
+#[test]
+fn test_to_xml_year_month_day() {
+    use crate::dinamic::Node;
+
+    let node = Node {
+        name: "data".into(),
+        value: Some((2025, 1, 1).into()),
+        attributes: Vec::default(),
+        children: Vec::default(),
+    };
+
+    assert_eq!(node.to_xml(), "<data>2025-01-01</data>");
+}
+
+#[test]
+fn test_to_xml_integer() {
+    use crate::dinamic::Node;
+
+    let node = Node {
+        name: "inteiro".into(),
+        value: Some(12.into()),
+        attributes: Vec::default(),
+        children: Vec::default(),
+    };
+
+    assert_eq!(node.to_xml(), "<inteiro>12</inteiro>");
+}
+
+#[test]
+fn test_to_xml_string() {
+    use crate::dinamic::Node;
+
+    let node = Node {
+        name: "nome".into(),
+        value: Some("Avelino Bego".into()),
+        attributes: Vec::default(),
+        children: Vec::default(),
+    };
+
+    assert_eq!(node.to_xml(), "<nome>Avelino Bego</nome>");
+}
+
+#[test]
+fn test_to_xml_none() {
+    use crate::dinamic::Node;
+
+    let node = Node {
+        name: "vazio".into(),
+        value: Some("".into()),
+        attributes: Vec::default(),
+        children: Vec::default(),
+    };
+
+    assert_eq!(node.to_xml(), "<vazio/>");
+}
+
+#[test]
+fn test_to_xml_float_attr() {
+    use crate::dinamic::Node;
+
+    let node = Node {
+        name: "salario".into(),
+        value: Some(10000.1234567890.into()),
+        attributes: vec![
+            ("tipo".into(), "mensal".into()),
+            ("mes".into(), "janeiro".into()),
+        ],
+        children: Vec::default(),
+    };
+
+    assert_eq!(
+        node.to_xml(),
+        r#"<salario tipo="mensal" mes="janeiro">10000.123456789</salario>"#
+    );
+}
+
+#[test]
+fn test_to_xml_empty() {
+    use crate::dinamic::Node;
+
+    let node = Node {
+        name: "sem_valor".into(),
+        value: None,
+        attributes: vec![],
+        children: Vec::default(),
+    };
+
+    assert_eq!(node.to_xml(), r#"<sem_valor></sem_valor>"#);
+}
+
+#[test]
+fn test_to_xml_float_attr_children() {
+    use crate::dinamic::Node;
+
+    let nome = Node {
+        name: "nome".into(),
+        value: Some("Avelino Bego".into()),
+        ..Default::default()
+    };
+
+    let profissao = Node {
+        name: "profissao".into(),
+        value: Some("Desenvolvedor".into()),
+        ..Default::default()
+    };
+
+    let node = Node {
+        name: "salario".into(),
+        value: None,
+        // attributes: vec![],
+        attributes: vec![
+            ("tipo".into(), "mensal".into()),
+            ("mes".into(), "janeiro".into()),
+            ("valor".into(), "10000.00".into()),
+        ],
+        children: vec![nome, profissao],
+    };
+    // println!("{}", node.to_xml());
+
+    assert_eq!(
+        node.to_xml(),
+        r#"<salario tipo="mensal" mes="janeiro" valor="10000.00"><nome>Avelino Bego</nome><profissao>Desenvolvedor</profissao></salario>"#
+    );
+}
 
 #[test]
 fn test_from_xml() {
     use crate::dinamic::Node;
-    use std::fs;
-    
-    let conteudo = fs::read_to_string("S1000.xml").expect("erro ao abrir arquivo");
-    let din: Node = conteudo.into();
-    
-    println!("{:#?}", din);
-}
+    use crate::dinamic::Values;
 
-#[test]
-fn test_to_xml_float(){
-    use crate::dinamic::Node;
-    
-    let node = Node{
-        name: "salario".into(),
-        value: 10000.1234567890.into(),
-        attributes: Vec::default(),
-        children: Vec::default(),
-    };
+    let node: Node = r#"<root><interno><nome>Avelino</nome></interno></root>"#.into();
 
-    println!("{}", node);
-}
-
-#[test]
-fn test_to_xml_year_month(){
-    use crate::dinamic::Node;
-    
-    let node = Node{
-        name: "data".into(),
-        value: (2025, 12).into(),
-        attributes: Vec::default(),
-        children: Vec::default(),
-    };
-
-    println!("{}", node);
-}
-
-#[test]
-fn test_to_xml_year_month_day(){
-    use crate::dinamic::Node;
-    
-    let node = Node{
-        name: "data".into(),
-        value: (2025, 12, 1).into(),
-        attributes: Vec::default(),
-        children: Vec::default(),
-    };
-
-    println!("{}", node);
-}
-
-#[test]
-fn test_to_xml_integer(){
-    use crate::dinamic::Node;
-    
-    let node = Node{
-        name: "inteiro".into(),
-        value: 12.into(),
-        attributes: Vec::default(),
-        children: Vec::default(),
-    };
-
-    println!("{}", node);
-}
-
-#[test]
-fn test_to_xml_string(){
-    use crate::dinamic::Node;
-    
-    let node = Node{
-        name: "nome".into(),
-        value: "Avelino Bego".into(),
-        attributes: Vec::default(),
-        children: Vec::default(),
-    };
-
-    println!("{}", node);
-}
-
-#[test]
-fn test_to_xml_none(){
-    use crate::dinamic::Node;
-    
-    let node = Node{
-        name: "vazio".into(),
-        value: "".into(),
-        attributes: Vec::default(),
-        children: Vec::default(),
-    };
-
-    println!("{}", node);
+    assert_eq!(
+        node.children[0].children[0].value,
+        Some(Values::String("Avelino".into()))
+    )
 }
